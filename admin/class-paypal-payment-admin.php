093d75800e2629833df76ae4406047dabda9ec0e
johnny manziel <johnny manziel@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-03-07 18:24:50 +0000

    add new version
    git-svn-id: https://plugins.svn.wordpress.org/paypal-payment/tags/1.0.1@1107736 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ admin/class-paypal-payment-admin.php
@@ -0,0 +1,57 @@
+<?php
+
+/**
+ * @class       MBJ_PayPal_Payment_Admin
+ * @version	1.0.0
+ * @package	paypal-payment
+ * @category	Class
+ * @author      johnny manziel <phpwebcreators@gmail.com>
+ */
+class MBJ_PayPal_Payment_Admin {
+
+    /**
+     * The ID of this plugin.
+     *
+     * @since    1.0.0
+     * @access   private
+     * @var      string    $plugin_name    The ID of this plugin.
+     */
+    private $plugin_name;
+
+    /**
+     * The version of this plugin.
+     *
+     * @since    1.0.0
+     * @access   private
+     * @var      string    $version    The current version of this plugin.
+     */
+    private $version;
+
+    /**
+     * Initialize the class and set its properties.
+     *
+     * @since    1.0.0
+     * @param      string    $plugin_name       The name of this plugin.
+     * @param      string    $version    The version of this plugin.
+     */
+    public function __construct($plugin_name, $version) {
+
+        $this->plugin_name = $plugin_name;
+        $this->version = $version;
+        $this->load_dependencies();
+    }
+
+    private function load_dependencies() {
+        /**
+         * The class responsible for defining all actions that occur in the Dashboard
+         */
+        require_once plugin_dir_path(dirname(__FILE__)) . 'admin/partials/paypal-payment-admin-display.php';
+
+        require_once plugin_dir_path(dirname(__FILE__)) . 'admin/partials/paypal-payment-general-setting.php';
+
+        require_once plugin_dir_path(dirname(__FILE__)) . 'admin/partials/paypal-payment-html-output.php';
+
+        require_once plugin_dir_path(dirname(__FILE__)) . 'admin/partials/paypal-payment-admin-widget.php';
+    }
+
+}
